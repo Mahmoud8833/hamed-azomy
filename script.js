60b9e57e3b44999c0243a7b87fcb5fafc56dc2c8
Mahmoud Abd alaziz <92608616+Mahmoud8833@users.noreply.github.com>
2024-09-30 00:44:11 +0300

    Add carousel

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.querySelectorAll('.close-modal').forEach((button) => {
 });
 
 const digital = document.querySelector('.digital');
-const topLeft = document.getElementById('top-left');
+const topLeft = document.getElementById('typogrphy');
 
 function whenClicked() {
   topLeft.addEventListener('click', (event) => {
@@ -56,4 +56,36 @@ function whenClicked() {
   });
 }
 
-whenClicked();
+const container = document.querySelector('.mainContainerCarouselFinal');
+const slide = document.querySelector('.slideTemplate');
+const prevBtn = document.querySelector(
+  '.controlerFinalCarousel div:nth-child(1)'
+);
+const nextBtn = document.querySelector(
+  '.controlerFinalCarousel div:nth-child(2)'
+);
+let nbclick = 0;
+const nbSlide = 10;
+const { style: containerStyle, getComputedStyle: getCS } = window;
+nextBtn.addEventListener('click', () => {
+  if (nbclick < nbSlide - 1) {
+    nbclick++;
+    updateScroll();
+  }
+});
+prevBtn.addEventListener('click', () => {
+  if (nbclick > 0) {
+    nbclick--;
+    updateScroll();
+  }
+});
+function updateScroll() {
+  const screenWidth = parseInt(
+    getCS(container).getPropertyValue('--screenWidth').replace('px', '')
+  );
+  const slideHeight = parseInt(
+    getCS(slide).getPropertyValue('--slideHeight').replace('px', '')
+  );
+  const scrollAmount = ((screenWidth - (slideHeight + 40)) / 2) * nbclick;
+  container.scroll({ left: scrollAmount, behavior: 'smooth' });
+}
