5f0bf3e09ee6fb8dd0710373b73b47669eb6316d
Mahmoud Abd alaziz <92608616+Mahmoud8833@users.noreply.github.com>
2024-10-02 16:14:23 +0300

    add carousel, tabs and cards

--- script.js
+++ script.js
@@ -56,36 +56,96 @@ function whenClicked() {
   });
 }
 
-const container = document.querySelector('.mainContainerCarouselFinal');
-const slide = document.querySelector('.slideTemplate');
-const prevBtn = document.querySelector(
-  '.controlerFinalCarousel div:nth-child(1)'
-);
-const nextBtn = document.querySelector(
-  '.controlerFinalCarousel div:nth-child(2)'
-);
-let nbclick = 0;
-const nbSlide = 10;
-const { style: containerStyle, getComputedStyle: getCS } = window;
-nextBtn.addEventListener('click', () => {
-  if (nbclick < nbSlide - 1) {
-    nbclick++;
-    updateScroll();
+// Carousel Start
+(function () {
+  'use strict';
+
+  var carousel = document.getElementsByClassName('carousel')[0],
+    slider = carousel.getElementsByClassName('carousel__slider')[0],
+    items = carousel.getElementsByClassName('carousel__slider__item'),
+    prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
+    nextBtn = carousel.getElementsByClassName('carousel__next')[0];
+
+  var width,
+    height,
+    totalWidth,
+    margin = 20,
+    currIndex = 0,
+    interval,
+    intervalTime = 4000;
+
+  function init() {
+    resize();
+    move(Math.floor(items.length / 2));
+    bindEvents();
+
+    timer();
   }
-});
-prevBtn.addEventListener('click', () => {
-  if (nbclick > 0) {
-    nbclick--;
-    updateScroll();
+
+  function resize() {
+    (width = Math.max(window.innerWidth * 0.25, 275)),
+      (height = window.innerHeight * 0.5),
+      (totalWidth = width * items.length);
+
+    slider.style.width = totalWidth + 'px';
+
+    for (var i = 0; i < items.length; i++) {
+      let item = items[i];
+      item.style.width = width - margin * 2 + 'px';
+      item.style.height = height + 'px';
+    }
   }
-});
-function updateScroll() {
-  const screenWidth = parseInt(
-    getCS(container).getPropertyValue('--screenWidth').replace('px', '')
-  );
-  const slideHeight = parseInt(
-    getCS(slide).getPropertyValue('--slideHeight').replace('px', '')
-  );
-  const scrollAmount = ((screenWidth - (slideHeight + 40)) / 2) * nbclick;
-  container.scroll({ left: scrollAmount, behavior: 'smooth' });
-}
+
+  function move(index) {
+    if (index < 1) index = items.length;
+    if (index > items.length) index = 1;
+    currIndex = index;
+
+    for (var i = 0; i < items.length; i++) {
+      let item = items[i],
+        box = item.getElementsByClassName('item__3d-frame')[0];
+      if (i == index - 1) {
+        item.classList.add('carousel__slider__item--active');
+        box.style.transform = 'perspective(1200px)';
+      } else {
+        item.classList.remove('carousel__slider__item--active');
+        box.style.transform =
+          'perspective(1200px) rotateY(' + (i < index - 1 ? 40 : -40) + 'deg)';
+      }
+    }
+
+    slider.style.transform =
+      'translate3d(' +
+      (index * -width + width / 2 + window.innerWidth / 2) +
+      'px, 0, 0)';
+  }
+
+  function timer() {
+    clearInterval(interval);
+    interval = setInterval(() => {
+      move(++currIndex);
+    }, intervalTime);
+  }
+
+  function prev() {
+    move(--currIndex);
+    timer();
+  }
+
+  function next() {
+    move(++currIndex);
+    timer();
+  }
+
+  function bindEvents() {
+    window.onresize = resize;
+    prevBtn.addEventListener('click', () => {
+      prev();
+    });
+    nextBtn.addEventListener('click', () => {
+      next();
+    });
+  }
+
+  init();
+})();
